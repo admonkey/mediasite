71e73cb68d114ac1da7ace898064d4c2fb9bc02d
Jeff <jpucket@puck.uark.edu>
2015-11-14 12:39:29 -0600

    change header links to use node relative root references

--- header.php
+++ header.php
@@ -1,23 +1,30 @@
-<?php include_once('credentials.php');?>
+<?php
+
+$relative_realpath = dirname(__FILE__) . "/";
+$relative_webpath = str_replace($_SERVER[DOCUMENT_ROOT], '', $relative_realpath);
+
+include_once($relative_realpath . 'credentials.php');
+
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" href="img/favicon.ico">
+	<link rel="icon" href="<?php echo $relative_webpath;?>img/favicon.ico">
 	<title><?php
     		if( !empty($pageTitle) )
 	    		echo $pageTitle . " | ";
 		    	echo $siteTitle;
 	?></title>
 
-	<!-- JQUERY --><script src="jquery/jquery.1.11.2.min.js"></script>
+	<!-- JQUERY --><script src="<?php echo $relative_webpath;?>jquery/jquery.1.11.2.min.js"></script>
 
 	<!-- BOOTSTRAP -->
-		<script src="bootstrap/bootstrap.3.3.4.min.js"></script>
-		<link rel="stylesheet" href="bootstrap/bootstrap.3.3.4.min.css"></link>
-		<link rel="stylesheet" href="bootstrap/bootstrap.custom.css"></link>
+		<script src="<?php echo $relative_webpath;?>bootstrap/bootstrap.3.3.4.min.js"></script>
+		<link rel="stylesheet" href="<?php echo $relative_webpath;?>bootstrap/bootstrap.3.3.4.min.css"></link>
+		<link rel="stylesheet" href="<?php echo $relative_webpath;?>bootstrap/bootstrap.custom.css"></link>
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
 			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -25,19 +32,19 @@
 		<![endif]-->
 
 	<!-- JQUERY-UI -->
-		<script src="jquery-ui/jquery-ui.1.11.4.min.js"></script>
-		<link rel="stylesheet" href="jquery-ui/jquery-ui.1.11.4.min.css">
+		<script src="<?php echo $relative_webpath;?>jquery-ui/jquery-ui.1.11.4.min.js"></script>
+		<link rel="stylesheet" href="<?php echo $relative_webpath;?>jquery-ui/jquery-ui.1.11.4.min.css">
 		<!-- <script src="//code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script> -->
 		<!-- <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.min.css"> -->
 
 	<!-- TABLESORTER -->
-		<script src="tablesorter/tablesorter.2.0.5b.min.js"></script>
-		<link rel="stylesheet" href="tablesorter/tablesorter.css">
+		<script src="<?php echo $relative_webpath;?>tablesorter/tablesorter.2.0.5b.min.js"></script>
+		<link rel="stylesheet" href="<?php echo $relative_webpath;?>tablesorter/tablesorter.css">
 
 	<!-- CHARTIST 
-		<script src="chartist/chartist.0.9.4.min.js"></script>
-		<link rel="stylesheet" href="chartist/chartist.min.css"></link>
-		<link rel="stylesheet" href="chartist/chartist.custom.css"></link>
+		<script src="<?php echo $relative_webpath;?>chartist/chartist.0.9.4.min.js"></script>
+		<link rel="stylesheet" href="<?php echo $relative_webpath;?>chartist/chartist.min.css"></link>
+		<link rel="stylesheet" href="<?php echo $relative_webpath;?>chartist/chartist.custom.css"></link>
 	-->
 
   </head>
@@ -51,16 +58,16 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.php"><?php echo $siteTitle; ?></a>
+          <a class="navbar-brand" href="<?php echo $relative_webpath;?>"><?php echo $siteTitle; ?></a>
         </div><!-- end navbar-header -->
 	<div id="navbar" class="collapse navbar-collapse">
 		<ul class="nav navbar-nav">
-			<li><a href="forms.php">Forms</a></li>
-			<li><a href="tables.php">Tables</a></li>
+			<li><a href="<?php echo $relative_webpath;?>forms.php">Forms</a></li>
+			<li><a href="<?php echo $relative_webpath;?>tables.php">Tables</a></li>
 		</ul>
 		<script>
 		  $('.navbar-nav').find('a').each(function(){
-		    if ($(this).attr("href") == "<?php echo basename($_SERVER['SCRIPT_NAME']);?>")
+		    if ($(this).attr("href") == "<?php echo $_SERVER['SCRIPT_NAME'];?>")
 		      $(this).parent().addClass("active");
 		  });
 		</script>
