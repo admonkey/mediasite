039e3d486b592e61aaa9f6800c516ddf53f62c8b
Jeff <jpucket@puck.uark.edu>
2015-11-14 13:28:57 -0600

    externalize sidenav.css

--- header.php
+++ header.php
@@ -25,6 +25,7 @@ include_once($relative_realpath . 'credentials.php');
 		<script src="<?php echo $relative_webpath;?>bootstrap/bootstrap.3.3.4.min.js"></script>
 		<link rel="stylesheet" href="<?php echo $relative_webpath;?>bootstrap/bootstrap.3.3.4.min.css"></link>
 		<link rel="stylesheet" href="<?php echo $relative_webpath;?>bootstrap/bootstrap.custom.css"></link>
+		<link rel="stylesheet" href="<?php echo $relative_webpath;?>bootstrap/sidenav.css"></link>
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
 			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -63,11 +64,11 @@ include_once($relative_realpath . 'credentials.php');
           </button>
 		  
 		  <ul class="nav navbar-nav">
-			<li><a href="#menu-toggle" id="menu-toggle">Sidenav</a></li>
+			<li><a href="#" id="sidenav-toggle">Sidenav</a></li>
 		  </ul>
-		    <!-- Menu Toggle Script -->
+		    <!-- Sidenav Toggle Script -->
 			<script>
-			$("#menu-toggle").click(function(e) {
+			$("#sidenav-toggle").click(function(e) {
 				e.preventDefault();
 				$("#wrapper").toggleClass("toggled");
 			});
@@ -93,135 +94,6 @@ include_once($relative_realpath . 'credentials.php');
 	  
     </nav>
 
-	
-<style>
-/*!
- * Start Bootstrap - Simple Sidebar HTML Template (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
-
-/* Toggle Styles */
-
-#wrapper {
-    padding-left: 0;
-    -webkit-transition: all 0.5s ease;
-    -moz-transition: all 0.5s ease;
-    -o-transition: all 0.5s ease;
-    transition: all 0.5s ease;
-}
-
-#wrapper.toggled {
-    padding-left: 250px;
-}
-
-#sidebar-wrapper {
-    z-index: 1000;
-    position: fixed;
-    left: 250px;
-    width: 0;
-    height: 100%;
-    margin-left: -250px;
-    overflow-y: auto;
-    background: #000;
-    -webkit-transition: all 0.5s ease;
-    -moz-transition: all 0.5s ease;
-    -o-transition: all 0.5s ease;
-    transition: all 0.5s ease;
-}
-
-#wrapper.toggled #sidebar-wrapper {
-    width: 250px;
-}
-
-#page-content-wrapper {
-    width: 100%;
-    position: absolute;
-    padding: 15px;
-}
-
-#wrapper.toggled #page-content-wrapper {
-    position: absolute;
-    margin-right: -250px;
-}
-
-/* Sidebar Styles */
-
-.sidebar-nav {
-    position: absolute;
-    top: 0;
-    width: 250px;
-    margin: 0;
-    padding: 0;
-    list-style: none;
-}
-
-.sidebar-nav li {
-    text-indent: 20px;
-    line-height: 40px;
-}
-
-.sidebar-nav li a {
-    display: block;
-    text-decoration: none;
-    color: #999999;
-}
-
-.sidebar-nav li a:hover {
-    text-decoration: none;
-    color: #fff;
-    background: rgba(255,255,255,0.2);
-}
-
-.sidebar-nav li a:active,
-.sidebar-nav li a:focus {
-    text-decoration: none;
-}
-
-.sidebar-nav > .sidebar-brand {
-    height: 65px;
-    font-size: 18px;
-    line-height: 60px;
-}
-
-.sidebar-nav > .sidebar-brand a {
-    color: #999999;
-}
-
-.sidebar-nav > .sidebar-brand a:hover {
-    color: #fff;
-    background: none;
-}
-
-@media(min-width:768px) {
-    #wrapper {
-        padding-left: 250px;
-    }
-
-    #wrapper.toggled {
-        padding-left: 0;
-    }
-
-    #sidebar-wrapper {
-        width: 250px;
-    }
-
-    #wrapper.toggled #sidebar-wrapper {
-        width: 0;
-    }
-
-    #page-content-wrapper {
-        padding: 20px;
-        position: relative;
-    }
-
-    #wrapper.toggled #page-content-wrapper {
-        position: relative;
-        margin-right: 0;
-    }
-}
-</style>
-	
 	
 	<div id="wrapper" class="toggled">
 
