69bbfc767c336b8f4b973eb08705f7a19c19857a
Jeff <jeff@jeffpuckett.com>
2015-11-15 21:47:51 -0600

    add git subtree notes

--- SubSection/index.php
+++ SubSection/index.php
@@ -3,6 +3,19 @@
 	require_once('../resources/header.php');
 ?>
 
-<h1>Welcome to <?php echo $pageTitle;?></h1>
+<h1>Git Subtree tips for a <?php echo $pageTitle;?></h1>
+
+<div class="well">
+
+<?php
+  $tips = file('../README/git-subtree-notes.txt');
+  foreach ($tips as $line_num => $line) {
+    if ($line[0] == "g")
+      echo "<pre>" . htmlspecialchars($line) . "</pre><br />\n";
+    else echo htmlspecialchars($line) . "<br />\n";
+  }
+?>
+
+</div><!-- /.well-->
 
 <?php require_once('../resources/footer.php');?>
