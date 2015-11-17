6f8f2b74094b8b0b5380b750f9ab12ca2291aa30
Jeff <jeff@jeffpuckett.com>
2015-11-16 22:13:25 -0600

    option to remove ssl certificate/key on uninstall

--- README/uninstall.bash
+++ README/uninstall.bash
@@ -11,14 +11,19 @@ sudo a2dissite $siteName && sudo service apache2 restart
 
 sudo rm "/etc/apache2/sites-available/"$siteName".conf"
 
-git checkout master
-git branch -D $siteName
-
-# rename cloned directory for virtual host
 # move to working directory
 cd $( dirname "${BASH_SOURCE[0]}" )
 cd ..
 currentGitDir=$(pwd)
+
+# remove ssl files
+rm -iv $currentGitDir/ssl/$siteName.crt
+rm -iv $currentGitDir/ssl/$siteName.key
+
+git checkout master
+git branch -D $siteName
+
+# rename cloned directory for virtual host
 vhostDirectory=$( cd .. && pwd )
 # option to abort
 echo -e "RENAMING $currentGitDir/ to $vhostDirectory/WebSite"
