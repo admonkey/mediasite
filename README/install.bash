4e50d3091697acd473b5794875bbe621aa775136
Jeff <jeff@jeffpuckett.com>
2015-11-17 21:24:09 -0600

    deny htaccess to ssl folder

--- README/install.bash
+++ README/install.bash
@@ -198,7 +198,6 @@ if $createSSL ; then
         if ! [ -d $sslDirectory ]; then
 	  mkdir $sslDirectory
 	fi
-	echo "deny from all" > $sslDirectory/.htaccess
 	openssl req -x509 -nodes -sha256 -days 365 -newkey rsa:2048 -keyout $sslDirectory/$siteName.key -out $sslDirectory/$siteName.crt -subj "/CN=$siteName/emailAddress=webmaster@$siteName"
 
 	# APPEND VIRTUAL HOST CONFIGURATION FILE
