bd5a857f41d0f6acd39452c8eb5a1815ee208339
Jeff <jpucket@puck.uark.edu>
2015-11-21 12:31:43 -0600

    separate data source table examples

--- Tables/index.php
+++ Tables/index.php
@@ -4,37 +4,24 @@
 ?>
 
 <div class="table-responsive well">
-  <h1>Sortable Table:</h1>
-  <table border=1>
-	 
-	 <thead>
-		<tr>
-		   <th>Column 1</th>
-		   <th>Column 2</th>
-		   <th>Column 3</th>
-		</tr>
-	 </thead>
-	 
-	 <tbody>
-		 <tr>
-			<td>value1a</td>
-			<td>value2a</td>
-			<td>value3a</td>
-		 </tr>
-		 <tr>
-			<td>value1b</td>
-			<td>value2b</td>
-			<td>value3b</td>
-		 </tr>
-		 <tr>
-			<td>value1c</td>
-			<td>value2c</td>
-			<td>value3c</td>
-		 </tr>
-	 </tbody>
-	 
-  </table>
+
+  <h1>Sortable Table</h1>
+
+  <?php // choose which table to load
   
+	switch ($_GET['table']) {
+    case "static":
+        include_once('table.static.html');
+        break;
+    case "mysql":
+        include_once('table.mysql.php');
+        break;
+	default:
+       include_once('table.static.html');
+	}
+	
+  ?>
+
 </div>
 
 <script>
---
+++ Tables/navigation-menu.php
@@ -0,0 +1,2 @@
+<li><a href="?table=static">Static HTML</a></li>
+<li><a href="?table=mysql">MySQL</a></li> 
