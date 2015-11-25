44414e79fea96d1b7922d364cf4cd29dd0e8c594
Jeff - wily@wilywolf <jeff@jeffpuckett.com>
2015-11-25 17:53:30 -0600

    polish html on subtree tips page

---
+++ SubTree/-resources/navigation-menu.php
@@ -0,0 +1,4 @@
+<li><a href='#create_subtree'>Create</a></li>
+<li><a href='#pull_subtree'>Pull</a></li>
+<li><a href='#backport_subtree'>Backport</a></li>
+<li><a href='#skipfile_subtree'>Skip</a></li>
--- SubTree/index.php
+++ SubTree/index.php
@@ -9,14 +9,79 @@ echo "<h1>$section_title Tips</h1>";
 
 <div class="well">
 
-<?php
-  $tips = file('git-subtree-notes.txt');
-  foreach ($tips as $line_num => $line) {
-    if ($line[0] == "g")
-      echo "<kbd>" . htmlspecialchars($line) . "</kbd><br />\n";
-    else echo $line . "<br />\n";
-  }
-?>
+<div id="create_subtree" class="well">
+
+	<h3>Create Sub Tree</h3>
+
+	<ol>
+		<li>
+			<p>add new remote pointing to plugin repo</p>
+			<p><kbd>git remote add plugin https://path.to/remotes/plugin.git</kbd></p>
+		</li>
+		<li>
+			<p>create subtree specifying the new folder prefix</p>
+			<p><kbd>git subtree add --prefix=plugins/demo plugin master</kbd></p>
+		</li>
+	</ol>
+
+</div><!-- /#create_subtree -->
+
+<div id="pull_subtree" class="well">
+
+	<h3>Pull Sub Tree Updates</h3>
+
+	<ol>
+		<li>
+			<p>pull normal commits made in plugin</p>
+			<p><kbd>git subtree pull --prefix=plugins/demo plugin master</kbd></p>
+		</li>
+	</ol>
+
+</div><!-- /#pull_subtree -->
+
+<div id="backport_subtree" class="well">
+
+	<h3>Backport Sub Tree Updates</h3>
+
+	<ol>
+		<li>
+			<p>commits made in superproject to be backported</p>
+			<p><kbd>git commit -am "new changes to be backported"</kbd></p>
+		</li>
+		<li>
+			<p>checkout new branch for merging, set to track subtree repo</p>
+			<p><kbd>git checkout -b backport plugin/master</kbd></p>
+		</li>
+		<li>
+			<p>cherry-pick backports</p>
+			<p><kbd>git cherry-pick -x --strategy=subtree master</kbd></p>
+		</li>
+		<li>
+			<p>push changes back to plugin source</p>
+			<p><kbd>git push plugin backport:master</kbd></p>
+		</li>
+	</ol>
+
+</div><!-- /#backport_subtree -->
+
+<div id="skipfile_subtree" class="well">
+
+	<h3>Stop Tracking Changes On a File</h3>
+
+	<ol>
+		<li>
+			<p>stop tracking changes for a file in repo</p>
+			<p><kbd>git update-index --skip-worktree [path/to/file]</kbd></p>
+		</li>
+		<li>
+			<p>start tracking changes for a file in repo again</p>
+			<p><kbd>git update-index --no-skip-worktree [path/to/file]</kbd></p>
+		</li>
+	</ol>
+
+</div><!-- /#skipfile_subtree -->
+
+<a href="https://medium.com/@porteneuve/mastering-git-subtrees-943d29a798ec" class="btn btn-default" target="_blank">Read More About Subtrees @porteneuve</a>
 
 </div><!-- /.well-->
 
