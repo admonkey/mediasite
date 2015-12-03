ad052ab3de6f87acd7812d80e41bf5bdb296f5d5
Jeff <jpucket@puck.uark.edu>
2015-12-02 20:35:55 -0600

    reorder main nav items

--- _resources/navigation-menu.php
+++ _resources/navigation-menu.php
@@ -1,10 +1,10 @@
 <?php echo "
 
-  <li><a href='$path_web_relative_root/Forms/'>Forms</a></li>
-  <li><a href='$path_web_relative_root/Forum/'>Forum</a></li>
-  <li><a href='$path_web_relative_root/Tables/'>Tables</a></li>
   <li><a href='$path_web_relative_root/SubTree/'>SubTree</a></li>
-  <li><a href='$path_web_relative_root/LDAP/'>LDAP</a></li>
+  <li><a href='$path_web_relative_root/Forum/'>Forum</a></li>
+  <li><a href='$path_web_relative_root/Forms/'>Forms</a></li>
+  <li><a href='$path_web_relative_root/Tables/'>Tables</a></li>
   <li><a href='$path_web_relative_root/FancyBox/'>FancyBox</a></li>
+  <li><a href='$path_web_relative_root/LDAP/'>LDAP</a></li>
 
 ";?>
