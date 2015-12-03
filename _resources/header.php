b380beffd858f32dfb526198c538842d326cae80
administrator@Waltahr <jpuckett@walton.uark.edu>
2015-12-03 16:00:27 -0600

    fix check if session already started

--- _resources/header.php
+++ _resources/header.php
@@ -4,7 +4,9 @@ if (isset($require_ssl) && $require_ssl) {
 	if(!isset($_SERVER['HTTPS'])) header('location: https://' . $_SERVER['SERVER_NAME'] . $_SERVER['SCRIPT_NAME'] . '?' . $_SERVER['QUERY_STRING']); 
 }
 
-session_start();
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 if (isset($_GET["logout"])){
   session_destroy();
   session_start();
