70b0b148664ae266a3ce88ec9d052231ea6c64b7
Jeff <jpucket@puck.uark.edu>
2015-12-02 19:56:34 -0600

    add stub Forum section

---
+++ Forum/index.php
@@ -0,0 +1,34 @@
+<?php
+
+include_once('../_resources/credentials.php');
+require_once('../_resources/header.php');
+
+echo "<h1>$section_title</h1>";
+
+// default login
+if (isset($_GET["default"])){
+	$_SESSION["user_id"] = 0;
+	$_SESSION["username"] = "Default";
+}
+if (!isset($_SESSION["username"]))
+	echo "<p><a href='?default' class='btn btn-primary'>Login as 'Default'</a></p>";
+
+?>
+
+
+<div id='message_div' class='well'>
+	
+	<form id='message_form' role='form' method='post'>
+
+		<div class='form-group'>
+			<label for='message_textarea'>Message:</label>
+			<textarea class='form-control' rows='5' id='message_textarea' name='message_textarea'></textarea>
+		</div>
+
+		<button type='submit' class='btn btn-primary'>Submit</button>
+
+	</form>
+
+</div>
+
+<?php require_once('../_resources/footer.php');?>
--- _resources/navigation-menu.php
+++ _resources/navigation-menu.php
@@ -1,6 +1,7 @@
 <?php echo "
 
   <li><a href='$path_web_relative_root/Forms/'>Forms</a></li>
+  <li><a href='$path_web_relative_root/Forum/'>Forum</a></li>
   <li><a href='$path_web_relative_root/Tables/'>Tables</a></li>
   <li><a href='$path_web_relative_root/SubTree/'>SubTree</a></li>
   <li><a href='$path_web_relative_root/LDAP/'>LDAP</a></li>
