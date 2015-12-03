399b2ef24337bd3550a3fa02baa010331e160b5b
Jeff <jpucket@puck.uark.edu>
2015-12-02 20:20:38 -0600

    set max length and autoexpand textarea to fit content

--- Forum/index.php
+++ Forum/index.php
@@ -21,7 +21,15 @@ else echo "
 
 		<div class='form-group'>
 			<label for='message_textarea'>Message:</label>
-			<textarea class='form-control' rows='5' id='message_textarea' name='message_textarea'></textarea>
+			<textarea class='form-control' style='width:100%' maxlength='140' rows='3' id='message_textarea' name='message_textarea'></textarea>
+			<!-- auto-expand textarea to fit content -->
+			<script>
+				$('textarea').keyup(function(e) {
+					while($(this).outerHeight() < this.scrollHeight + parseFloat($(this).css('borderTopWidth')) + parseFloat($(this).css('borderBottomWidth'))) {
+						$(this).height($(this).height()+1);
+					};
+				});
+			</script>
 		</div>
 
 		<button type='submit' class='btn btn-primary'>Submit</button>
