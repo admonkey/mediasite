a85e24e939cfc7071a5c1228fbf7818391d71b43
administrator@Waltahr <jpuckett@walton.uark.edu>
2015-12-04 09:57:14 -0600

    change highlight color to blue to match style

--- Forum/index.php
+++ Forum/index.php
@@ -58,7 +58,7 @@ if( !empty($mysql_connection) ){
 	  var thread_id = row.find("message_data").attr("thread_id");
 	  var thread_name = row.find("message_data").attr("thread_name");
 	  $.ajax({url: "Forum.messages.ajax.php?thread_id=" + thread_id, success: function(result){
-	    $("#thread_div").hide().html(result).prepend("<h2>" + thread_name + "</h2>").show("highlight");
+	    $("#thread_div").hide().html(result).prepend("<h2>" + thread_name + "</h2>").show("highlight", {color: '#99FFFF'});
 	  },cache: false});
       }).hover( function() {
 	  $(this).toggleClass("hover");
