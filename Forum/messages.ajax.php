0f31a73c3e398f7cb3ede2d8355b2fa24d44db49
Jeff <jeff@jeffpuckett.com>
2015-12-06 14:59:24 -0600

    only show delete button if current user

--- Forum/messages.ajax.php
+++ Forum/messages.ajax.php
@@ -41,8 +41,13 @@ if( !empty($mysql_connection) ){
 			<div class='col-md-3'><div class='message_metadata'>
 				<message_data message_id='$row[message_id]'></message_data>
 				<p><label class='label label-primary'>message_author_user_id $row[message_author_user_id]</label></p>
-				<p><label class='label label-info'>$row[message_creation_time]</label></p>
-				<p><a href='javascript:void(0)' onclick='delete_message($row[message_id], $(this))'><label class='label label-danger'>Delete</label></a></p>
+				<p><label class='label label-info'>$row[message_creation_time]</label></p>";
+				
+				// delete button
+				if ($row["message_author_user_id"] == $_SESSION["user_id"]) echo "
+				<p><a href='javascript:void(0)' onclick='delete_message($row[message_id], $(this))'><label class='label label-danger'>Delete</label></a></p>";
+				
+			echo "
 			</div><!-- /.message_metadata --></div><!-- /.col-md-3 -->
 			
 		</div><!-- /.row --></div><!-- /.well -->\n
