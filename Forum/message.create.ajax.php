0e2a3cf0de46abcf2702ce4219f324355bc4e7d6
Jeff <jeff@jeffpuckett.com>
2015-12-06 15:51:30 -0600

    encapsulate message.read.inc

--- Forum/message.create.ajax.php
+++ Forum/message.create.ajax.php
@@ -48,25 +48,7 @@ if( !empty($mysql_connection) ){
     ";
     $result = mysql_query($sql) or die(mysql_error());
 
-    // data
-    while ($row = mysql_fetch_assoc($result)){
-		echo "
-		<div class='well message_well'><div class='row'>
-
-			<div class='col-md-9'>
-				<p>$row[message_text]</p>
-			</div><!-- /.col-md-9 -->
-			
-			<div class='col-md-3'><div class='message_metadata'>
-				<message_data thread_id='$row[message_thread_id]' message_id='$row[message_id]'></message_data>
-				<p><label class='label label-primary'>message_author_user_id $row[message_author_user_id]</label></p>
-				<p><label class='label label-info'>$row[message_creation_time]</label></p>
-				<p><a href='javascript:void(0)' onclick='delete_message($row[message_id], $(this))'><label class='label label-danger'>Delete</label></a></p>
-			</div><!-- /.message_metadata --></div><!-- /.col-md-3 -->
-			
-		</div><!-- /.row --></div><!-- /.well -->\n
-		";
-	 }
+    include("message.read.inc.php");
 
 } else {
 
