3fe48c47f1825a0280a7ea94e8b69de7c51e374b
administrator@Waltahr <jpuckett@walton.uark.edu>
2015-12-07 14:46:49 -0600

    fix: show message inner HTML in editor box

--- Forum/index.php
+++ Forum/index.php
@@ -159,7 +159,7 @@ if (!isset($_SESSION["user_id"])) { ?>
     } else {
       var message_editor = $("#message_editor").clone();
       message_editor.find("[name=message_id]").val(element.parents(".message_metadata").find("message_data").attr("message_id"));
-      message_editor.find("textarea").val(message_body_well.find(".message_text").text());
+      message_editor.find("textarea").val(message_body_well.find(".message_text").prop('innerHTML'));
 		message_editor_well.html(message_editor.show());
       message_body_well.hide("slide", function(){message_editor_well.show("slide")});
     }
