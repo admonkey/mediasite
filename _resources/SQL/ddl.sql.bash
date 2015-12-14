842b1cd9512e23d4b8ffbbf8022b5cd110db5964
administrator@Waltahr <jpuckett@walton.uark.edu>
2015-12-14 17:17:57 -0600

    refactor with comments

--- _resources/SQL/ddl.sql.bash
+++ _resources/SQL/ddl.sql.bash
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-ddl_files=( \
-  "Tables/_resources/SQL/Object.ddl.sql"\
+# must be in proper order for drop/add with key relationships
+sql_files=( \
   "Forum/_resources/SQL/Forum.drop.sql"\
   "Login/Database/_resources/SQL/Users.drop.sql"\
   "Login/Database/_resources/SQL/Users.ddl.sql"\
   "Forum/_resources/SQL/Forum.ddl.sql"\
   "Forum/_resources/SQL/Forum.procedures.sql"\
+  "Tables/_resources/SQL/Object.ddl.sql"\
 )
 
 # move to working directory
 cd $( dirname "${BASH_SOURCE[0]}" )
 cd ../..
 
-for ddl in "${ddl_files[@]}"
+for sql in "${ddl_files[@]}"
 do
-  mysql --host=localhost --user=username --password=p@55W0rd --database=example_database < $ddl
+  mysql --host=localhost --user=username --password=p@55W0rd --database=example_database < $sql
 done
